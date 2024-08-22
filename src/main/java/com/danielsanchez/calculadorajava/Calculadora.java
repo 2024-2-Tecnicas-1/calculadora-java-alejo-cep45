e42614575c7b48c15f5072799d5bf57f98739d25
Juan Acosta <juanestebanacostarodriguez@gmail.com>
2024-08-22 08:30:07 -0500

    Se agregó el case de módulo y potencia

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -26,6 +26,15 @@ public class Calculadora {
                 // Operación división:
                 respuesta = (double) numero1 / numero2;
                 break;
+            case '^' :
+                // Operación división:
+                respuesta = (double) Math.pow(numero1 , numero2);
+                break;
+            
+            case '%' :
+                // Operación división:
+                respuesta = (double) numero1 % numero2;
+                break;
             
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
                 
@@ -46,7 +55,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /)");
+        System.out.println("Ingrese la operación (+, -, *, /, %, ^)");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
